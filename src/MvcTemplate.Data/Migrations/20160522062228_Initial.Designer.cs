27ddd3a17191af605fcb8f225d01a30079665fe0
Muchiachio <aldis.rameika@gmail.com>
2020-04-10 09:28:06 +0300

    3rd party libraries update
    - Updated 3rd party libraries to their newest versions.

--- src/MvcTemplate.Data/Migrations/20160522062228_Initial.Designer.cs
+++ src/MvcTemplate.Data/Migrations/20160522062228_Initial.Designer.cs
@@ -17,7 +17,7 @@ namespace MvcTemplate.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.2")
+                .HasAnnotation("ProductVersion", "3.1.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
